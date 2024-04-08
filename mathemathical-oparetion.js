8fcc77b7255e34638ef3558c3f0cd7d52090d4b0
miftahujannat <miftahurimny@gmail.com>
2024-04-08 11:46:45 +0600

    added

--- mathemathical-oparetion.js
+++ mathemathical-oparetion.js
@@ -1,7 +1,17 @@
 var onionprice = 43;
 var eggprice = 42;
+var eggquantity = 5;
 var totalprice = onionprice + eggprice;
-console.log(onionprice);
-console.log(eggprice);
-console.log(onionprice + eggprice);
-console.log(totalprice);
+var priceDifference = onionprice - eggprice;
+var eggpricemultiplication = eggprice * eggquantity;
+// console.log(onionprice);
+// console.log(eggprice);
+// console.log(onionprice + eggprice);
+// console.log(totalprice);
+// console.log(priceDifference);
+// console.log(eggpricemultiplication);
+
+var moneyavailable = 100;
+var orangeprice = 5;
+var moneyquntity = moneyavailable / orangeprice;
+console.log(moneyquntity);

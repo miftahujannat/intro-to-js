2acc0915d9236860a849998321e1f617a6476e53
miftahujannat <miftahurimny@gmail.com>
2024-04-08 16:05:36 +0600

    added

--- conversion.js
+++ conversion.js
@@ -13,5 +13,17 @@ var fullname = firstname + '.'+lastname;
 
 var onionprice = '42';
 var eggprice = '41';
-var totalprice =onionprice +' ' +eggprice;
-console.log(totalprice);
+var totalprice =onionprice + eggprice;
+// console.log(totalprice);
+
+// integer
+var sunglass = 3;
+// float 
+var price = 99.99;
+
+var onionprice = '42.50';
+var eggprice = '41.30';
+var onionpricenumber = parseInt(onionprice);
+var eggpricenumber = parseFloat(eggprice);
+console.log(eggpricenumber)
+// console.log(onionpricenumber);

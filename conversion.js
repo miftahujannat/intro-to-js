6d7a5ce83ecbeb92a3ae6a98c31a9186cb7ba7cb
miftahujannat <miftahurimny@gmail.com>
2024-04-08 13:23:13 +0600

    added

---
+++ conversion.js
@@ -0,0 +1,17 @@
+var firstname = 'Miftahu';
+var secondname = 'Jannat';
+var thirdname = 'Rimni';
+
+// ''aita diye space dei 
+var fullname = firstname +' ' +secondname +' ' +thirdname;
+// console.log(fullname);
+
+var firstname = 'Md';
+var lastname = 'Shimul';
+var fullname = firstname + '.'+lastname;
+// console.log(fullname);
+
+var onionprice = '42';
+var eggprice = '41';
+var totalprice =onionprice +' ' +eggprice;
+console.log(totalprice);

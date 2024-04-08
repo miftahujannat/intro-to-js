3c2ce5d25ee4ac6f569cdfba03067f85e3e18fbe
miftahujannat <miftahurimny@gmail.com>
2024-04-08 12:51:05 +0600

    added

---
+++ math-oparetion.js
@@ -0,0 +1,35 @@
+var price1 = 30;
+var price2 = 10;
+var sum = price1 + price2;
+var difference = price2 - price1;
+var multiplication =price1 * price2;
+var devision =price1 / price2;
+// console.log(sum);
+// console.log(difference);
+// console.log(multiplication);
+// console.log(devision);
+
+var doubleprice = price2 * 20;
+// var newprice = price1 + 10;
+// price1 = price1 + 10;
+price1 += 10;
+
+// price2 = price2 - 2;
+price2 -= 2;
+// console.log(doubleprice);
+// console.log(newprice);
+// console.log(price1);
+// console.log(price2);
+
+var age = 14;
+// age = age - 1;
+// age -= 1;
+age--;
+// console.log(age);
+
+var valobasha = 99;
+valobasha = valobasha + 1
+valobasha += 1
+valobasha ++;
+console.log(valobasha);
+

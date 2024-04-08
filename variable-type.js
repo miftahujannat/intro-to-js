f4a5675baace90c8e73f7425f8f39c5723fb0e0a
miftahujannat <miftahurimny@gmail.com>
2024-04-08 16:34:00 +0600

    added

---
+++ variable-type.js
@@ -0,0 +1,20 @@
+var tshirtprice = 200;
+var tshirtprice2 = '200';
+var israining = true;
+var isromantic;
+
+// console.log(typeof tshirtprice);
+// console.log(typeof tshirtprice2);
+// console.log(typeof israining);
+// console.log(typeof isromantic);
+
+var today =new Date();
+console.log(typeof today);
+
+// special edition 
+var firstnumber = 0.1;
+var secondnumber = 0.2;
+var total = firstnumber + secondnumber;
+total = total.toFixed(1);
+total = parseFloat(total);
+console.log(total);
